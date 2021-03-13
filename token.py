b52309085be8735b8dad39e8316dd6e6b3cc2e03
Zenux <77491495+zenux-dev@users.noreply.github.com>
2021-03-13 16:34:36 +0200

    Delete token.py

--- token.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def check_token(token):
-    response = requests.get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})
-    check_token.result = response.status_code
